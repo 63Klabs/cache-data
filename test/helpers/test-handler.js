93b35c29ff2c063746717687e95e14f091e41718
ChadKluck <chadkluck@gmail.com>
2026-01-09 15:16:49 -0600

    updated documentation to remove references to old Lambda handler callback

--- test/helpers/test-handler.js
+++ test/helpers/test-handler.js
@@ -1,10 +1,10 @@
 const { ClientRequest, Response } = require("../../src/lib/tools");
 
-exports.handler = function( event, context, callback ) {
+exports.handler = function( event, context ) {
 
     const req = new ClientRequest(event, context);
 
     const resp = new Response(req, {statusCode: 200, headers: {}, body: "hello!"});
 
-    callback( null, resp.finalize() );
+    return resp.finalize();
 }
